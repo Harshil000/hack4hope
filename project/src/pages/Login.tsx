48f05f044a6aec2879066d09c2f4f3e0380cd2a7
Harshil000 <techshock01@gmail.com>
2025-04-26 14:09:50 +0530

    login page done

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -1,18 +1,25 @@
 import React, { useId } from 'react'
 import { useState } from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 const Login = () => {
+    const { loginWithRedirect } = useAuth0();
     const [adminLogin, setAdminLogin] = useState('user-type');
     return (
-        <div>
-            <div className='mt-7 flex items-center justify-center gap-4'>
+        <div className={adminLogin == 'user-type' ? 'flex flex-col items-center w-full' : 'justify-center items-center w-full flex flex-col'}>
+            {adminLogin == 'user-type' ? <div className='w-[60%] mt-6 border bg-red-300 border-red-600 rounded-lg h-8 flex items-center px-4'>
+                    Please Select type of user to login
+                </div>
+            : null}
+            <div className={adminLogin == 'user-type' ? 'mt-7 flex items-center gap-4 w-[60%]' : "mt-7 flex items-center gap-4"}>
                 <h2>I AM :- </h2>
                 <div className='flex items-center justify-center gap-2'>
                     {/* <input type='radio' className='hidden' name='user-type' id='user' /> */}
-                    <label htmlFor={'user'} onClick={() => { setAdminLogin('user') }} className={adminLogin == 'user' ? `btn-primary w-32 h-16 text-2xl` : 'w-32 h-16 flex items-center justify-center text-lg cursor-pointer border border-[rgb(13,148,136)] rounded-lg text-neutral-800'}>User</label>
+                    <label htmlFor={'user'} onClick={() => { setAdminLogin('user') }} className={adminLogin == 'user' ? `btn-primary w-32 h-12 text-2xl` : 'w-32 h-12 flex items-center justify-center text-lg cursor-pointer border border-[rgb(13,148,136)] rounded-lg text-neutral-800'}>User</label>
                     {/* <input type='radio' className='hidden' name='user-type' id='admin' /> */}
-                    <label htmlFor='admin' onClick={() => { setAdminLogin('admin') }} className={adminLogin == 'admin' ? "btn-primary w-48 h-16 text-2xl" : "w-48 h-16 flex items-center justify-center text-lg cursor-pointer border border-[rgb(13,148,136)] rounded-lg text-neutral-800"}>Admin</label>
+                    <label htmlFor='admin' onClick={() => { setAdminLogin('admin') }} className={adminLogin == 'admin' ? "btn-primary w-48 h-12 text-2xl" : "w-48 h-12 flex items-center justify-center text-lg cursor-pointer border border-[rgb(13,148,136)] rounded-lg text-neutral-800"}>Admin</label>
                 </div>
             </div>
+            <button className={adminLogin == 'user-type' ? 'hidden' :'btn-primary mt-5'} onClick={() => loginWithRedirect()}>Log In</button>
         </div>
     )
 }
