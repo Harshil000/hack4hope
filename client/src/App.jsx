d451d266e9149100e5909451a9c48d25fb1eb019
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-04-26 11:26:42 +0530

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Components/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '../../client/src/context/ThemeContext';
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
+import BookingPage from './pages/BookingPage';
+import UserDashboard from './pages/UserDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import NotFound from './pages/NotFound';
 
-const App = () => {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Home />} path='/' />
-            </Routes>
-        </BrowserRouter>
-    )
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="booking" element={<BookingPage />} />
+            <Route path="dashboard" element={<UserDashboard />} />
+            <Route path="admin" element={<AdminDashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
